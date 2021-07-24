b26c9c0a2eb8c5f7087a7c94500c147443b8de26
DanDiDanD <pfbarrett98@gmail.com>
2021-07-23 22:36:32 -0500

    mejoras home

--- src/components/HomeCarousel/HomeCarousel.js
+++ src/components/HomeCarousel/HomeCarousel.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
+import './HomeCarousel.scss'
 
 export default () => (
   <Carousel autoPlay
-    className="carousel-home" 
-    dynamicHeight={true}  
+    className="carousel-home"
+    // dynamicHeight={true}
     infiniteLoop={true}
-    interval={5000}
+    interval={6000}
     showThumbs={false}
     >
     <div>
-      <img alt="" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB15poFh.img?h=1080&w=1920&m=6&q=60&o=t&l=f" />
+      <img className="carousel-img" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB15poFh.img?h=1080&w=1920&m=6&q=60&o=t&l=f" />
     </div>
     <div>
-      <img alt="" src="https://www.krisporelmundo.com/wp-content/uploads/2016/03/20140618_210420.jpg" />
+      <img className="carousel-img" src="http://www.cedna.org/es/wp-content/uploads/2018/07/cocina-peruana-cabecera.jpg" />
+    </div>
+    <div>
+      <img className="carousel-img" src="https://www.revistalabarra.com/wp-content/uploads/2019/11/comida-peruana-1500x800.jpg" />
     </div>
   </Carousel>
 );
